07968b06717556cbabd209dd37875a647dabbcab
Richard Clare <richard.clare@upr.edu>
2021-05-17 16:50:56 -0400

    Create package.R
    #This function will load all of the Solana & Serum R functions into the R environment.

---
+++ package.R
@@ -0,0 +1,7 @@
+#This function will load all of the Solana & Serum R functions into the R environment.
+
+source("solana_balances.R")
+source("solana_stats.R")
+source("serum_buy.R")
+
+#authenticate here
