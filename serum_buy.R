9eaa6312797a1be2e915ff5a67de55cb28ee7adc
Richard Clare <richard.clare@upr.edu>
2021-05-17 16:49:42 -0400

    Create serum_buy.R
    This function should be able to execute a buy order on the serum dex after authenticating with the server.

---
+++ serum_buy.R
@@ -0,0 +1,5 @@
+#This function should be able to execute a buy order on the serum dex after authenticating with the server.
+
+serum_buy = function(pair,amount,tolerance) { 
+  return("buy order")
+}
